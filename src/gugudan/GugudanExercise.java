b3e6885dd59035ed6ca534922a1bc8a9182a6310
Sophia HJ Lee <Sophia HJ Lee@SophiaHJLee-PC>
2018-06-05 17:34:27 +0900

    innitial commit

---
+++ src/gugudan/GugudanExercise.java
@@ -0,0 +1,23 @@
+package gugudan;
+
+import java.util.Scanner;
+
+public class GugudanExercise {
+
+	public static void main(String[] args) {
+		
+		System.out.println("출력하고 싶은 단을 입력하세요:");
+		
+		Scanner sc = new Scanner(System.in);
+		int inDan = sc.nextInt();
+			
+		System.out.println();
+		
+		for (int i = 1; i < 10; i++) {
+			
+			int result = inDan * i;
+			System.out.println(inDan + " * " + i + " = " + result);
+		}
+
+	}
+ }
